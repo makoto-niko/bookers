6bf48bf167af14c52a3e9161554899537e704e52
EC2 Default User <ec2-user@ip-172-31-52-69.ec2.internal>
2024-02-05 05:48:32 +0000

    create機能途中

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,26 +1,27 @@
 class BooksController < ApplicationController
   def new
-    @book = Book.new  
-  end
- 
-  def create
-    book = Book(book_params)
-    book.save
-    redirect_to '/top'
+    @book = Book.new
   end
 
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to book_path(book.id)
+  end
+  
   def index
+    @books = Book.all
+    @book = Book.new  
   end
 
   def show
-    
   end
 
   def edit
   end
   
   private
-  def list_params
+  def book_params
     params.require(:book).permit(:title, :body)
   end
 end
